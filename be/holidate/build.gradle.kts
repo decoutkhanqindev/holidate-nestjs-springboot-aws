41754d13585b13d7d1e7919218303e26a0b2f523
Minh Khang <phamminhkhangpy2003@gmail.com>
2025-09-06 20:53:02 +0700

    feat: setup project env

---
+++ be/holidate/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.gradle.kotlin.dsl.annotationProcessor
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.webapp"
+version = "0.0.1-SNAPSHOT"
+description = "hotel booking web app"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+  implementation("io.github.cdimascio:dotenv-java:3.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+  implementation ("org.mapstruct:mapstruct:1.6.3")
+  annotationProcessor ("org.mapstruct:mapstruct-processor:1.6.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ be/holidate/src/test/java/com/webapp/holidate/HolidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.holidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
