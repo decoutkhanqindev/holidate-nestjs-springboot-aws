f611b1360068c4152d179f7db8f32e41676e3d64
Minh Khang <phamminhkhangpy2003@gmail.com>
2025-10-25 00:17:12 +0700

    feat: add discount mapping and retrieval functionality

--- be/holidate/src/main/java/com/webapp/holidate/mapper/booking/DiscountMapper.java
+++ be/holidate/src/main/java/com/webapp/holidate/mapper/booking/DiscountMapper.java
@@ -1,7 +1,12 @@
 package com.webapp.holidate.mapper.booking;
 
+import com.webapp.holidate.dto.request.booking.DiscountCreationRequest;
+import com.webapp.holidate.dto.response.booking.DiscountResponse;
+import com.webapp.holidate.entity.booking.discount.Discount;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DiscountMapper {
+  Discount toEntity(DiscountCreationRequest request);
+  DiscountResponse toDiscountResponse(Discount discount);
 }
--- be/holidate/src/main/java/com/webapp/holidate/service/booking/DiscountService.java
+++ be/holidate/src/main/java/com/webapp/holidate/service/booking/DiscountService.java
@@ -1,11 +1,16 @@
 package com.webapp.holidate.service.booking;
 
+import com.webapp.holidate.dto.request.booking.DiscountCreationRequest;
+import com.webapp.holidate.dto.response.booking.DiscountResponse;
+import com.webapp.holidate.mapper.booking.DiscountMapper;
 import com.webapp.holidate.repository.booking.discount.DiscountRepository;
 import com.webapp.holidate.repository.booking.discount.HotelDiscountRepository;
 import com.webapp.holidate.repository.booking.discount.SpecialDayDiscountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
@@ -15,5 +20,21 @@ public class DiscountService {
   HotelDiscountRepository hotelDiscountRepository;
   SpecialDayDiscountRepository specialDayDiscountRepository;
 
+  DiscountMapper mapper;
 
+//  public DiscountResponse create(DiscountCreationRequest request){
+//
+//  }
+
+  public List<DiscountResponse> getAll() {
+    return discountRepository.findAll().stream()
+      .map(mapper::toDiscountResponse)
+      .toList();
+  }
+
+  public DiscountResponse getByCode(String code) {
+    return discountRepository.findByCode(code)
+      .map(mapper::toDiscountResponse)
+      .orElse(null);
+  }
 }
