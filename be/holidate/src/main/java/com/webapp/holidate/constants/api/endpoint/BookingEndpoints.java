68ee2865323bac981251633a17208cab7db40c04
Minh Khang <phamminhkhangpy2003@gmail.com>
2025-11-01 13:27:33 +0700

    fix: update check-in and check-out endpoint paths to use hyphenated format for consistency

--- be/holidate/src/main/java/com/webapp/holidate/constants/api/endpoint/BookingEndpoints.java
+++ be/holidate/src/main/java/com/webapp/holidate/constants/api/endpoint/BookingEndpoints.java
@@ -6,8 +6,8 @@ public class BookingEndpoints {
   public static final String PRICE_PREVIEW = "/price-preview";
   public static final String CANCEL = "/cancel";
   public static final String RESCHEDULE = "/reschedule";
-  public static final String CHECKIN = "/checkin";
-  public static final String CHECKOUT = "/checkout";
+  public static final String CHECKIN = "/check-in";
+  public static final String CHECKOUT = "/check-out";
   public static final String PAYMENT = "/payment";
   public static final String CALLBACK = "/callback";
 }
